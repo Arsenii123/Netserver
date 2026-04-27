af13c85746464d0108d22da6f7cabca7530bc83f
Ars123 <@arsenii310810gmail.com>
2026-04-27 16:42:03 +0300

    New

--- Program.cs
+++ Program.cs
@@ -12,7 +12,8 @@ class Program
     static readonly ConcurrentDictionary<string, DateTime> Clients = new();
 
     // строка подключения к PostgreSQL
-    static string ConnString = "Host=localhost;Username=postgres;Password=123;Database=chat";
+    static string ConnString = "Host=localhost;Port=5432;Username=postgres;Password=123;Database=chat;Encoding=UTF8";
+
 
     static async Task Main()
     {
